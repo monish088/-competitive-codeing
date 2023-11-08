bb1cf98e44189c6b713fc20fe2faf618fceb817d
monish088 <100213349+monish088@users.noreply.github.com>
2023-11-08 10:41:16 +0530

    Time: 2 ms (83.13%), Space: 40.9 MB (10.92%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i)=='{'||s.charAt(i)=='('||s.charAt(i)=='['){
+                st.push(s.charAt(i));
+            } else if(!st.isEmpty()&&(s.charAt(i)=='}'||s.charAt(i)==')'||s.charAt(i)==']')){
+                if(s.charAt(i)=='}' && st.peek()=='{'){
+                    st.pop();
+                } else if(s.charAt(i)==')' && st.peek()=='('){
+                    st.pop();
+                } else if(s.charAt(i)==']' && st.peek()=='['){
+                    st.pop();
+            } else{
+                return false;
+            }
+            } else{
+                return false;
+            }
+            
+        }
+
+         return(st.isEmpty());
+    }
+   
+}
