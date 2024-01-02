b10f31a0d7a58921e7e14d1861f8c88f8418a7c8
monish088 <100213349+monish088@users.noreply.github.com>
2024-01-02 11:08:30 +0530

    Time: 2 ms (11.83%) | Memory: 57.2 MB (80.70%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,8 +3,7 @@ class Solution {
         int maxSoFar = Integer.MIN_VALUE, maxending = 0;
         for(int i = 0; i < nums.length; i++){
             maxending = maxending + nums[i];
-            if(maxSoFar < maxending)
-                maxSoFar = maxending;
+           maxSoFar = Math.max(maxSoFar, maxending);
             if(maxending < 0)
                 maxending = 0;
             
