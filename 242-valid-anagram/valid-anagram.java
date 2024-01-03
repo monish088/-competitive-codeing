65ca9a83a2ec5b7c33669d26c8dbfc7b5ee0d82a
monish088 <100213349+monish088@users.noreply.github.com>
2024-01-03 11:06:20 +0530

    Time: 25 ms (7.24%) | Memory: 44.9 MB (6.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,22 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
-
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        // String nS = arr1.toString();
-        // String nT = arr1.toString();
-
-        // if(nS.equals(nT)){
-        //     return true;
-        // } else{
+        // if(s.length() != t.length()){
         //     return false;
         // }
-        return Arrays.equals(arr1,arr2);
+        // char[] arr1 = s.toCharArray();
+        // char[] arr2 = t.toCharArray();
+
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
+        // return Arrays.equals(arr1,arr2);
+
+         Map<Character, Integer> charCountMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        }
+
+        // Subtract character occurrences in the second string
+        for (char c : t.toCharArray()) {
+            int count = charCountMap.getOrDefault(c, 0);
+            if (count == 0) {
+                // Character not present in the first string
+                return false;
+            }
+            charCountMap.put(c, count - 1);
+        }
+
+        // Check if all character counts are zero
+        for (int count : charCountMap.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
