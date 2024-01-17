ebbc16442ee4859f9f293cf0b13d41d090559919
monish088 <100213349+monish088@users.noreply.github.com>
2024-01-17 17:07:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (11.79%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,16 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        if(nums.length == 0){
-            return 0;
-        }
-        int k = 0;
+          int k = 0; // Variable to track the number of elements not equal to val
 
-        for(int i=0; i<nums.length;i++){
-            if(nums[i]!=val){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
                 nums[k] = nums[i];
                 k++;
             }
         }
+
         return k;
     }
 }
