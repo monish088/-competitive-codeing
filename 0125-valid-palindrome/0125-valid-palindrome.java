2624d4a816aadaef62ad4d7f0edeb8a2b90fc581
monish088 <100213349+monish088@users.noreply.github.com>
2024-01-30 12:36:00 +0530

    Time: 15 ms (33.52%), Space: 44.9 MB (27.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-         StringBuilder cleanString = new StringBuilder();
-        
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                cleanString.append(Character.toLowerCase(c));
-            }
-        }
-        
-        int left = 0, right = cleanString.length() - 1;
-        
-        while (left < right) {
-            if (cleanString.charAt(left) != cleanString.charAt(right)) {
-                return false; // Not a palindrome
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
             }
             left++;
             right--;
         }
-        
-        return true; // Palindrome
+        return true;
     }
 }
