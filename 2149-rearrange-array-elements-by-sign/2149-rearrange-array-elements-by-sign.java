287105cbca0f994a6e383e1a47d34d5f71418f98
monish088 <100213349+monish088@users.noreply.github.com>
2024-02-15 12:04:23 +0530

    Time: 3 ms (100%), Space: 77 MB (48.58%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,35 +1,51 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer> positiveNums = new ArrayList<>();
-        List<Integer> negativeNums = new ArrayList<>();
+        // List<Integer> positiveNums = new ArrayList<>();
+        // List<Integer> negativeNums = new ArrayList<>();
 
-        // Separate positive and negative integers
-        for (int num : nums) {
-            if (num > 0) {
-                positiveNums.add(num);
+        // // Separate positive and negative integers
+        // for (int num : nums) {
+        //     if (num > 0) {
+        //         positiveNums.add(num);
+        //     } else {
+        //         negativeNums.add(num);
+        //     }
+        // }
+
+        // int[] rearrangedNums = new int[nums.length];
+        // int i = 0, j = 0, k = 0;
+
+        // // Merge positive and negative integers
+        // while (i < positiveNums.size() && j < negativeNums.size()) {
+        //     rearrangedNums[k++] = positiveNums.get(i++);
+        //     rearrangedNums[k++] = negativeNums.get(j++);
+        // }
+
+        // // Append any remaining positive or negative integers
+        // while (i < positiveNums.size()) {
+        //     rearrangedNums[k++] = positiveNums.get(i++);
+        // }
+
+        // while (j < negativeNums.size()) {
+        //     rearrangedNums[k++] = negativeNums.get(j++);
+        // }
+
+        // return rearrangedNums;
+
+        int[] ans = new int[nums.length];
+
+        int p = 0;
+        int n = 1;
+
+        for(int num : nums){
+            if(num > 0){
+                ans[p] = num;
+                p += 2;
             } else {
-                negativeNums.add(num);
+                ans[n] = num;
+                n += 2;
             }
         }
-
-        int[] rearrangedNums = new int[nums.length];
-        int i = 0, j = 0, k = 0;
-
-        // Merge positive and negative integers
-        while (i < positiveNums.size() && j < negativeNums.size()) {
-            rearrangedNums[k++] = positiveNums.get(i++);
-            rearrangedNums[k++] = negativeNums.get(j++);
-        }
-
-        // Append any remaining positive or negative integers
-        while (i < positiveNums.size()) {
-            rearrangedNums[k++] = positiveNums.get(i++);
-        }
-
-        while (j < negativeNums.size()) {
-            rearrangedNums[k++] = negativeNums.get(j++);
-        }
-
-        return rearrangedNums;
+        return ans;
     }
 }
