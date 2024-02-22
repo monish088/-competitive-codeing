1d11f0c7ad6c0a6eeec06fde993a624799f5cf05
monish088 <100213349+monish088@users.noreply.github.com>
2024-02-22 13:30:00 +0530

    Time: 1 ms (100%), Space: 46 MB (58.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,29 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int k = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){
-                nums[k] = nums[i];
-                k++;
+        // int k = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] != 0){
+        //         nums[k] = nums[i];
+        //         k++;
+        //     }
+        // }
+        // for(int i = k; i < nums.length; i++){
+        //     nums[i] = 0;
+        // }
+         // Initialize a pointer to keep track of the position to place the next non-zero element
+        int insertPos = 0;
+        
+        // Iterate through the array
+        for (int num : nums) {
+            // If the current element is non-zero, move it to the current insert position and increment the insert position
+            if (num != 0) {
+                nums[insertPos++] = num;
             }
         }
-        for(int i = k; i < nums.length; i++){
-            nums[i] = 0;
-        }
+        
+        // Fill the remaining positions with zeros
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
     }
+}
 }
