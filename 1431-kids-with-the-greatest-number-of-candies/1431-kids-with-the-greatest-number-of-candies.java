17a9641841b34284ab060c190277f86272e954eb
monish088 <100213349+monish088@users.noreply.github.com>
2024-02-22 13:10:49 +0530

    Time: 1 ms (87.14%), Space: 42.3 MB (61.34%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,17 +1,31 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-         // Find the maximum number of candies among all kids
-        int maxCandies = Arrays.stream(candies).max().getAsInt();
+        //  // Find the maximum number of candies among all kids
+        // int maxCandies = Arrays.stream(candies).max().getAsInt();
         
-        // Initialize a boolean array to store the result
-        List<Boolean> result = new ArrayList<>();
+        // // Initialize a boolean array to store the result
+        // List<Boolean> result = new ArrayList<>();
         
-        // Iterate through each kid's candies
-        for (int i = 0; i < candies.length; i++) {
-            // Check if the current kid can have the greatest number of candies
-            result.add(candies[i] + extraCandies >= maxCandies);
+        // // Iterate through each kid's candies
+        // for (int i = 0; i < candies.length; i++) {
+        //     // Check if the current kid can have the greatest number of candies
+        //     result.add(candies[i] + extraCandies >= maxCandies);
+        // }
+        
+        // return result;
+        int max = -1;
+
+        for(int a : candies){
+            if(a > max){
+                max = a;
+            }
         }
-        
-        return result;
+
+        List<Boolean> list = new ArrayList<>();
+
+        for(int i = 0; i < candies.length; i++){
+            list.add(candies[i]+extraCandies >= max);
+        }
+        return list;
     }
 }
