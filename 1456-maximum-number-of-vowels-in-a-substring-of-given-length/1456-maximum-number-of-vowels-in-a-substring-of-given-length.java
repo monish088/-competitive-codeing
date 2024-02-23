d2dc53fef404696a742d6007a3f78d6b7b9c20eb
monish088 <100213349+monish088@users.noreply.github.com>
2024-02-23 14:33:21 +0530

    Time: 22 ms (40.71%), Space: 44.4 MB (84.85%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -5,10 +5,28 @@ class Solution {
 
         Set<Character> vowels = new HashSet<>();
 
-        set.add('a');
-         set.add('a');
-          set.add('a');
-           set.add('a');
-            set.add('a');
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+
+        for(int i = 0; i < k; i++){
+            if(vowels.contains(s.charAt(i))){
+                windowVowels++;
+            }
+            maxVowels = windowVowels;
+        }
+        
+        for(int i = k; i < s.length(); i++){
+            if(vowels.contains(s.charAt(i-k))){
+                windowVowels--;
+            }
+            if(vowels.contains(s.charAt(i))){
+                windowVowels++;
+            }
+            maxVowels = Math.max(maxVowels, windowVowels);
+        }
+        return maxVowels;
     }
 }
