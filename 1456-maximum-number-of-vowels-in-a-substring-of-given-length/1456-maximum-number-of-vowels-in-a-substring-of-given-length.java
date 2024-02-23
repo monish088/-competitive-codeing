00fe41a43aa8e8e976e56ca609360aca04d05749
monish088 <100213349+monish088@users.noreply.github.com>
2024-02-23 14:28:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,29 +1,14 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int maxCount = 0;
-        int count = 0;
-        // Count vowels in the first substring of length k
-        for (int i = 0; i < k; i++) {
-            if (isVowel(s.charAt(i))) {
-                count++;
-            }
-        }
-        maxCount = count;
+        int maxVowels = 0;
+        int windowVowels = 0;
 
-        // Slide the window and update the count of vowels
-        for (int i = k; i < s.length(); i++) {
-            if (isVowel(s.charAt(i - k))) {
-                count--; // Remove the leftmost character from the previous window
-            }
-            if (isVowel(s.charAt(i))) {
-                count++; // Add the current character to the window
-            }
-            maxCount = Math.max(maxCount, count);
-        }
+        Set<Character> vowels = new HashSet<>();
 
-        return maxCount;
-    }
-    private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        set.add('a');
+         set.add('a');
+          set.add('a');
+           set.add('a');
+            set.add('a');
     }
 }
