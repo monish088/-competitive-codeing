6c07a6096926160838df075c1b3251fdc367a4ee
monish088 <100213349+monish088@users.noreply.github.com>
2024-02-25 17:12:41 +0530

    Time: 0 ms (100%), Space: 44.9 MB (6.13%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,24 +10,45 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-         if (head == null || head.next == null) return head;
+        //  if (head == null || head.next == null) return head;
 
-        ListNode oddHead = head;
-        ListNode evenHead = head.next;
-        ListNode odd = oddHead;
-        ListNode even = evenHead;
+        // ListNode oddHead = head;
+        // ListNode evenHead = head.next;
+        // ListNode odd = oddHead;
+        // ListNode even = evenHead;
 
-        // Traverse the original list, splitting nodes into odd and even lists
-        while (even != null && even.next != null) {
-            odd.next = even.next;
-            odd = odd.next;
-            even.next = odd.next;
-            even = even.next;
+        // // Traverse the original list, splitting nodes into odd and even lists
+        // while (even != null && even.next != null) {
+        //     odd.next = even.next;
+        //     odd = odd.next;
+        //     even.next = odd.next;
+        //     even = even.next;
+        // }
+
+        // // Combine the odd and even lists
+        // odd.next = evenHead;
+
+        // return oddHead;
+
+        if(head == null){
+            return head;
         }
 
-        // Combine the odd and even lists
+        ListNode odd = head;
+        ListNode even = head.next;
+
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null){
+
+            odd.next = odd.next.next;
+            odd = odd.next;
+
+            even.next = even.next.next;
+            even = even.next;
+        }
         odd.next = evenHead;
 
-        return oddHead;
+        return head;
     }
 }
