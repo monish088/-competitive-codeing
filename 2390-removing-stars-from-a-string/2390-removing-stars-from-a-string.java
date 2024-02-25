74b7afb6d9318fb5981630e2fb9c86709ea01abf
monish088 <100213349+monish088@users.noreply.github.com>
2024-02-25 16:30:52 +0530

    Time: 35 ms (82.59%), Space: 45.4 MB (84.62%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,23 +1,37 @@
 class Solution {
     public String removeStars(String s) {
-        Stack<Character> stack = new Stack<>();
+        // Stack<Character> stack = new Stack<>();
+
+        // for (char c : s.toCharArray()) {
+        //     if (c == '*') {
+        //         // Remove the closest non-star character to the left (if it exists)
+        //         if (!stack.isEmpty() && stack.peek() != '*') {
+        //             stack.pop();
+        //         }
+        //     } else {
+        //         stack.push(c);
+        //     }
+        // }
+
+        // StringBuilder result = new StringBuilder();
+        // for (char c : stack) {
+        //     result.append(c);
+        // }
+
+        // return result.toString(); 
+        StringBuilder result = new StringBuilder();
 
         for (char c : s.toCharArray()) {
             if (c == '*') {
                 // Remove the closest non-star character to the left (if it exists)
-                if (!stack.isEmpty() && stack.peek() != '*') {
-                    stack.pop();
+                if (result.length() > 0 && result.charAt(result.length() - 1) != '*') {
+                    result.deleteCharAt(result.length() - 1);
                 }
             } else {
-                stack.push(c);
+                result.append(c);
             }
         }
 
-        StringBuilder result = new StringBuilder();
-        for (char c : stack) {
-            result.append(c);
-        }
-
-        return result.toString(); 
+        return result.toString();
     }
 }
