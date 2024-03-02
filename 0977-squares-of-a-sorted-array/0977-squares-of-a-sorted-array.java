5780ea45691f760c2e599a4f17088ec73e393f0c
monish088 <100213349+monish088@users.noreply.github.com>
2024-03-02 17:53:55 +0530

    Time: 1 ms (100%), Space: 46.8 MB (71.21%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,17 +1,35 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] res = new int[nums.length];
-        int left = 0;
-        int right = nums.length - 1;
-        for(int i = nums.length - 1; i >= 0; i--){
-            if(Math.abs(nums[left]) > Math.abs(nums[right])){
-                res[i] = nums[left] * nums[left];
+        // int[] res = new int[nums.length];
+        // int left = 0;
+        // int right = nums.length - 1;
+        // for(int i = nums.length - 1; i >= 0; i--){
+        //     if(Math.abs(nums[left]) > Math.abs(nums[right])){
+        //         res[i] = nums[left] * nums[left];
+        //         left++;
+        //     } else {
+        //         res[i] = nums[right] * nums[right];
+        //         right--;
+        //     }
+        // }
+        // return res;
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        int index = n - 1;
+        
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare > rightSquare) {
+                result[index--] = leftSquare;
                 left++;
             } else {
-                res[i] = nums[right] * nums[right];
+                result[index--] = rightSquare;
                 right--;
             }
         }
-        return res;
+        
+        return result;
     }
 }
