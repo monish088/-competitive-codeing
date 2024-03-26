7c0e5163e2eb0e305fa325d2be0fe9eb05f5d25d
monish088 <100213349+monish088@users.noreply.github.com>
2024-03-26 15:13:47 +0530

    Time: 1 ms (98%), Space: 55.1 MB (43.1%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Cyclic sort
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        // Find the smallest positive integer missing
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1; 
+    }
+}
